3ab4dd5c1603c77c48726e85119966388dbc515b
Ravindra_Reddy <reddyravindrat4512@gmail.com>
2023-11-25 14:54:09 +0530

    added output to output folder

--- src/server/test/testMatchesPerYear.js
+++ src/server/test/testMatchesPerYear.js
@@ -3,4 +3,14 @@ const matchesPerSeason = require("../matchesPerYear");
 
 const result = matchesPerSeason(matches);
 
-console.log(result);
+//console.log(result);
+
+// printing output
+
+const fs = require("fs");
+
+const output = "../../public/output/problem.json";
+
+const resultJSON = JSON.stringify(result);
+
+fs.writeFileSync(output, resultJSON);
